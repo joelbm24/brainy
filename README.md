41d7e116f90e0bcb79a7958cf14e1d38c4fdd3ef
Joel Moore <joelbm24@gmail.commit>
2012-02-05 22:01:49 -0800

    restructerd

---
+++ README.md
@@ -0,0 +1,21 @@
+===========
+Brainy
+===========
+
+Brainy is a brainfuck interpreter and repl written in python.
+
+Setup
+=========
+
+Install:
+    $ sudo python setup.py install
+
+
+Running
+========
+To run type:
+    $ brainy
+
+    or
+
+    $ brainy <file name>
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Brainy',
+    version='0.1.1',
+    author='Joel Buchheim-Moore',
+    author_email='joelbm24@gmail.com',
+    scripts=['bin/brainy'],
+    package_dir={'brainy': 'lib'},
+    packages=['brainy'],
+    url='http://github.com/joelbm24/brainy',
+    license='LICENSE.txt',
+    description='brainfuck interpreter',
+    long_description=open('README.txt').read(),
+)
